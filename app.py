e0ff372804a3db08597f158b4fa30b5bcc6721a8
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-11-15 22:44:38 +0530

    Made the route for quiz

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def home():
+    questionPage = False
     if request.method == 'POST':
         prompt = request.form.get('prompt')
-        print(prompt)
-    return render_template("index.html")
+        if prompt:
+            questionPage = True
+    return render_template("index.html",questionPage = questionPage)
+
+@app.route("/questionPage",methods=['GET','POST'])
+def quiz():
+    return render_template("quiz.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
