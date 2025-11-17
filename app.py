f54c2363095ce624ce82ce986ba50eaa58de6d45
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-11-17 14:07:34 +0530

    Server host

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def score():
                                         total = len(session.get('questions',[])))
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False,host='0.0.0.0')
