37b5744038688ce89ae63dde66691943b85ba74d
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-11-17 10:27:40 +0530

    No need

--- static/js/quiz.js
+++
@@ -1,8 +0,0 @@
-const submitButton = document.querySelector('.submit');
-
-// Listen for the submit button to be clicked
-submitButton.addEventListener('click', (event) => {
-    // Prevent the form from actually submitting (which refreshes the page)
-    event.preventDefault(); 
-
-});
