03f742743f9b37f8b3b31297c45956cb32b763ab
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2026-01-08 13:10:15 +0530

    build Evaluation agent

---
+++ agents/EvaluationAgent.py
@@ -0,0 +1,33 @@
+from langchain.agents import create_agent
+from langchain_groq import ChatGroq
+from langchain.messages import SystemMessage,HumanMessage
+from langgraph.checkpoint.memory import InMemorySaver
+import os
+from dotenv import load_dotenv
+load_dotenv()
+    
+sysMsg = '''
+You are an expert at evaluating a candidate's performance in a mock test.
+you are given a list of questions and the answers provided by the candidate.
+'''
+
+class Agent:
+    def __init__(self):
+        os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY") #type:ignore
+        self.model = ChatGroq(
+            model="openai/gpt-oss-120b",
+            temperature=0.8,
+        )
+        self.agent = create_agent(
+            self.model,
+            system_prompt=SystemMessage(sysMsg),
+            checkpointer=InMemorySaver()
+        )
+        
+        
+    def evaluate(self,input:str):
+        result = self.agent.invoke({"messages": HumanMessage(input)},
+                            config={"configurable": {"thread_id": "1"}}
+                        )
+        return result['messages'][-1].content
+    
